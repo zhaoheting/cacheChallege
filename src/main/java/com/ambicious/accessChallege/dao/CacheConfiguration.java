02f3cdbb575c65ba5acd11564ab20b96439efcbc
Heting.Zhao <heting.zhao@openjawtech.com>
2019-06-21 15:34:59 +0800

    cache learn

--- src/main/java/com/ambicious/accessChallege/dao/CacheConfiguration.java
+++ src/main/java/com/ambicious/accessChallege/dao/CacheConfiguration.java
@@ -1,26 +1,39 @@
 package com.ambicious.accessChallege.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @FunctionalInterface
 public interface CacheConfiguration {
+    Logger logger = LoggerFactory.getLogger(CacheConfiguration.class);
 
+    /**
+     * @return
+     */
     RedisConnectionFactory getRedisConnectionFactory();
 
+    /**
+     * @return
+     */
     default RedisTemplate<String, Object> getRedisTemplate() {
         RedisTemplate<String, Object> template = null;
         try {
             RedisConnectionFactory factory = getRedisConnectionFactory();
-            template = new RedisTemplate<>();
-            template.setConnectionFactory(factory);
-            template.setKeySerializer(new StringRedisSerializer());
-            template.setValueSerializer(new StringRedisSerializer());
-            template.setHashKeySerializer(new StringRedisSerializer());
-            template.setHashValueSerializer(new StringRedisSerializer());
+            if (factory != null) {
+                template = new RedisTemplate<>();
+                template.setConnectionFactory(factory);
+                template.setKeySerializer(new StringRedisSerializer());
+                template.setValueSerializer(new StringRedisSerializer());
+                template.setHashKeySerializer(new StringRedisSerializer());
+                template.setHashValueSerializer(new StringRedisSerializer());
+            } else {
+                logger.error("Failed to connect to redis.");
+            }
         } catch (Exception e) {
-
+            logger.error("Failed to connect to redis.", e);
         }
         return template;
     }
